389ad07a29977544ee8216b6ffc369ea09a53290
Valerie <valeriesham13@gmail.com>
2023-09-22 10:30:21 -0400

    Categories model str test added

---
+++ store/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name="django", slug='django')
+
+    def test_category_model_entry(self):
+        """
+        Test Category model default name
+        """
+
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
