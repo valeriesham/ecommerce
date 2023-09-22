8fad7684ec8803b92e2dfa47b62c33918eefff46
Valerie <valeriesham13@gmail.com>
2023-09-22 10:42:25 -0400

    Products model str test added

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from store.models import Category, Product
-
+from django.contrib.auth.models import User
 
 class TestCategoriesModel(TestCase):
 
@@ -9,10 +9,27 @@ class TestCategoriesModel(TestCase):
 
     def test_category_model_entry(self):
         """
-        Test Category model default name
+        Test product model data insertion/types/field attributes
         """
 
         data = self.data1
         self.assertEqual(str(data), 'django')
 
 
+class TestProductsModel(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(
+            category_id=1, title='django beginners', created_by_id=1,
+            slug='django-beginners', price='20.00', image='django.img'
+        )
+
+    def test_products_model_entry(self):
+        """
+        Test product model data insertion/types/field attributes
+        """
+
+        data = self.data1
+        self.assertEqual(str(data), 'django beginners')
